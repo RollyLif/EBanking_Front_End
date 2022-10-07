98ef36bb5531bc8e90e4a460f65919590ce6c874
RollyLif <relifungula@gmail.com>
2022-10-07 13:19:26 +0200

    style: adding a search bar

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { catchError, Observable, throwError } from 'rxjs';
 import { Customer } from '../model/customer.model';
 import { CustomerService } from '../services/customer.service';
@@ -11,16 +12,24 @@ import { CustomerService } from '../services/customer.service';
 })
 export class CustomersComponent implements OnInit {
   customers! : Observable<Array<Customer>>;
-  errorMessage! : string; 
-  constructor(private customerService: CustomerService) { }
+  errorMessage! : string;
+  searchFormGroup! : FormGroup;
+  constructor(private customerService: CustomerService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.searchFormGroup=this.fb.group({
+      keyword: this.fb.control("")
+    })
     this.customers = this.customerService.getCustomers().pipe(
       catchError(err => {
         this.errorMessage = err.message;
         return throwError(err);
       })
     );
+  }
+
+  handleSearchCustomers(){
+    
   }
 
 }
