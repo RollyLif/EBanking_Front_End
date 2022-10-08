e1a500a1cd2ff29b609be118a2000448aeb7db40
RollyLif <relifungula@gmail.com>
2022-10-08 21:30:52 +0200

    feature: adding a new customer

--- src/app/new-customer/new-customer.component.ts
+++ src/app/new-customer/new-customer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Customer } from '../model/customer.model';
+import { CustomerService } from '../services/customer.service';
 
 @Component({
   selector: 'app-new-customer',
@@ -8,7 +10,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class NewCustomerComponent implements OnInit {
   newCustomerFormGroup! : FormGroup;
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private customerService:CustomerService) { }
 
   ngOnInit(): void {
     this.newCustomerFormGroup=this.fb.group({
@@ -18,7 +20,16 @@ export class NewCustomerComponent implements OnInit {
   }
 
   handleSaveCustomer(){
-    
+    let customer:Customer=this.newCustomerFormGroup.value;
+    this.customerService.saveCustomers(customer).subscribe({
+      next: data =>{
+        alert("Customer has been successfully saved!")
+      },
+      error : err => {
+        console.log(err);
+      }
+    })
+
   }
 
 }
