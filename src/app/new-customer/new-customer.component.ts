51b0052c903f1cde2441f57cad311d8bfdee5866
RollyLif <relifungula@gmail.com>
2022-10-09 07:42:01 +0200

    features: validation of name and email by angular

--- src/app/new-customer/new-customer.component.ts
+++ src/app/new-customer/new-customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Customer } from '../model/customer.model';
 import { CustomerService } from '../services/customer.service';
 
@@ -14,8 +14,8 @@ export class NewCustomerComponent implements OnInit {
 
   ngOnInit(): void {
     this.newCustomerFormGroup=this.fb.group({
-      name : this.fb.control(""),
-      email : this.fb.control("")
+      name : this.fb.control("",[Validators.required, Validators.minLength(3)]),
+      email : this.fb.control("", [Validators.required, Validators.email])
     });
   }
 
