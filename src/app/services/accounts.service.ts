9ff9010a0b7c48e3b8685007aab2da63b680643c
RollyLif <relifungula@gmail.com>
2022-10-10 15:03:58 +0200

    adding model for accounts

--- src/app/services/accounts.service.ts
+++ src/app/services/accounts.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAccount(accountId : string, page : number, size: number){
+    return this.http.get(environment.backendhost+"/accounts/"+accountId+"pageOperations?page="+page+"&size="+size);
+  }
 }
