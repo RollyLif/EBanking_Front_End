dfde457326a25a23d0df715758c848c2fcaf620f
RollyLif <relifungula@gmail.com>
2022-10-11 15:06:15 +0200

    style: displaying account with account's operations

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AccountDetails } from '../model/account.model';
+import { AccountsService } from '../services/accounts.service';
 
 @Component({
   selector: 'app-accounts',
@@ -8,18 +11,28 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class AccountsComponent implements OnInit {
 
-  accountFormGroup! : FormGroup;
 
-  constructor(private fb : FormBuilder) { }
+  accountFormGroup! : FormGroup;
+  currentPage : number = 0;
+  pageSize : number = 5;
+  accountObservable! : Observable<AccountDetails>
+
+  constructor(private fb : FormBuilder, private accountService : AccountsService) { }
 
   ngOnInit(): void {
     this.accountFormGroup = this.fb.group({
       accountId : this.fb.control(''),
     });
   }
-  
+
   handleSearchAccount() {
-    
+    let accountId : string = this.accountFormGroup.value.accountId;
+    this.accountObservable=this.accountService.getAccount(accountId, this.currentPage, this.pageSize);
+  }
+
+  gotoPage(page: number) {
+    this.currentPage = page;
+    this.handleSearchAccount()
   }
 
 }
--- src/app/model/account.model.ts
+++ src/app/model/account.model.ts
@@ -4,7 +4,7 @@ export interface AccountDetails {
     currentPage:          number;
     totalPages:           number;
     pageSize:             number;
-    accountOperations: AccountOperation[];
+    accountOperationDTOs: AccountOperation[];
 }
 
 export interface AccountOperation {
